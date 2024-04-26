3312f24854265a9d599f574a59819cdf6388b69a
Diego Santana <3c.santana@gmail.com>
2024-04-25 23:18:40 -0300

    refactor: refatora o dto para exibição do relatório de vendas

--- src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -3,23 +3,34 @@ package com.devsuperior.dsmeta.dto;
 import java.time.LocalDate;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SaleMinProjection;
 
 public class SaleMinDTO {
 
 	private Long id;
 	private Double amount;
 	private LocalDate date;
+	private String name;
 	
-	public SaleMinDTO(Long id, Double amount, LocalDate date) {
+	public SaleMinDTO(Long id, Double amount, LocalDate date, String seller) {
 		this.id = id;
 		this.amount = amount;
 		this.date = date;
+		this.name = seller;
 	}
 	
 	public SaleMinDTO(Sale entity) {
 		id = entity.getId();
 		amount = entity.getAmount();
 		date = entity.getDate();
+		name = entity.getSeller().getName();
+	}	
+	
+	public SaleMinDTO(SaleMinProjection projection) {
+		id = projection.getId();
+		amount = projection.getAmount();
+		date = projection.getDate();
+		name = projection.getName();
 	}
 
 	public Long getId() {
@@ -32,5 +43,9 @@ public class SaleMinDTO {
 
 	public LocalDate getDate() {
 		return date;
+	}
+
+	public String getSeller() {
+		return name;
 	}
 }
