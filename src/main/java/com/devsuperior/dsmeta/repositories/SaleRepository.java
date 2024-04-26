b06ba0ebf71e4ec89ceb7cd0181098465f7e94e1
Diego Santana <3c.santana@gmail.com>
2024-04-25 23:19:36 -0300

    feat: criação do método para o  o relatório de vendas

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.projections.SaleMinProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query(nativeQuery = true, value = 
+            "SELECT tb_sales.id, tb_sales.date, tb_sales.amount ,tb_seller.name " +
+            "FROM tb_sales " +
+            "JOIN tb_seller ON tb_sales.seller_id = tb_seller.id " +
+            "WHERE tb_sales.date BETWEEN :minDate AND :maxDate " +
+            "AND UPPER(tb_seller.name) LIKE UPPER(CONCAT('%',:name, '%'))")
+    Page<SaleMinProjection> salesReport(String minDate, String maxDate, String name, Pageable pageable);
 }
