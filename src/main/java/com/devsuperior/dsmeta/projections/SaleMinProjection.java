ca1f1a6cc694e7f7934f351f2c6d5cc206910129
Diego Santana <3c.santana@gmail.com>
2024-04-25 23:19:06 -0300

    feat: criação da projection

---
+++ src/main/java/com/devsuperior/dsmeta/projections/SaleMinProjection.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dsmeta.projections;
+
+import java.time.LocalDate;
+
+public interface SaleMinProjection {
+    Long getId();
+    Double getAmount();
+    LocalDate getDate();
+    String getName();
+}
