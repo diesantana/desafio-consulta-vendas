8a7aef3676b0807325d09b85d80fa72cba72b929
Diego Santana <3c.santana@gmail.com>
2024-04-26 21:22:31 -0300

    fix: corrige o nome do recurso na response

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dsmeta.repositories;
 
-import com.devsuperior.dsmeta.dto.SaleMinDTO;
-import com.devsuperior.dsmeta.projections.SaleMinProjection;
+import com.devsuperior.dsmeta.projections.SaleReportProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,10 +11,10 @@ import org.springframework.data.jpa.repository.Query;
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     @Query(nativeQuery = true, value = 
-            "SELECT tb_sales.id, tb_sales.date, tb_sales.amount ,tb_seller.name " +
+            "SELECT tb_sales.id, tb_sales.date, tb_sales.amount ,tb_seller.name AS sellerName " +
             "FROM tb_sales " +
             "JOIN tb_seller ON tb_sales.seller_id = tb_seller.id " +
             "WHERE tb_sales.date BETWEEN :minDate AND :maxDate " +
-            "AND UPPER(tb_seller.name) LIKE UPPER(CONCAT('%',:name, '%'))")
-    Page<SaleMinProjection> salesReport(String minDate, String maxDate, String name, Pageable pageable);
+            "AND UPPER(tb_seller.name) LIKE UPPER(CONCAT('%',:sellerName, '%'))")
+    Page<SaleReportProjection> salesReport(String minDate, String maxDate, String sellerName, Pageable pageable);
 }
